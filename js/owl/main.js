c9fd5b26f4c9bff08b1826470618e8aa4e6d2552
gravits-r <vanessa.univesp@gmail.com>
2021-07-14 13:46:48 -0300

    Exercicio desafio clone Netflix

---
+++ js/owl/main.js
@@ -0,0 +1,7 @@
+function botao2(){
+    window.open("https://www.themoviedb.org/movie/286217-the-martian?language=pt-BR");
+}
+
+function botao1(){
+    window.open("https://youtu.be/QVX5DSzRn14");
+}
